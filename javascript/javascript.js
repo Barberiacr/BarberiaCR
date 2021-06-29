a92d1f3a5d199dc41f6de6728fb728b18ad01937
Barberiacr <78170463+Barberiacr@users.noreply.github.com>
2021-06-29 17:51:18 -0300

    Add files via upload

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,6 +1,16 @@
-function lista(){
-    while(i<10)
-    {
-        i++;
+(function () {
+    //reloj
+    const hora =document.getElementById("hora");
+    const minutos =document.getElementById("minutos");
+
+    function actualizarReloj(){
+        let date=new Date;
+        hora.textContent=date.getHours();
+        minutos.textContent=date.getMinutes();
     }
-}
+    actualizarReloj();
+    setInterval(() => {
+        actualizarReloj();
+    }, 60000);
+    
+}());
